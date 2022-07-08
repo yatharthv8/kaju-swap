f0a87df15746f4f70983133cff576e8335dceb68
your_username <yatharthverma.che18@iitbhu.ac.in>
2022-07-08 17:06:08 +0530

    Github Pages deployment setup

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:yatharthv8/kaju-swap.git main:gh-pages
+git push -f git@github.com:yatharthv8.github.io/kaju-swap.git main:gh-pages
 
 cd -
